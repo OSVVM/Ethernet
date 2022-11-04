4406e236b296b55f9ba51604548c68e680bb2122
Jim Lewis <jim@synthworks.com>
2022-11-04 10:08:48 -0700

    Changed SetAlertLogName to SetTestName

--- TestStandAlone/Tb_xMii1.vhd
+++ TestStandAlone/Tb_xMii1.vhd
@@ -52,7 +52,7 @@ begin
   ControlProc : process
   begin
     -- Initialization of test
-    SetAlertLogName("Tb_xMii1") ;
+    SetTestName("Tb_xMii1") ;
     SetLogEnable(PASSED, TRUE) ;    -- Enable PASSED logs
     SetLogEnable(INFO, TRUE) ;    -- Enable INFO logs
 
